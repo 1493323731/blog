dc6d252006af4ba3be02cf9bf1085c611a74dd88
1493323731 <1493323731@qq.com>
2022-12-24 00:04:58 +0800

    finish

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -29,11 +29,11 @@ const router = useRouter();
 let rules = {
     account: [
         { required: true, message: "请输入账号", trigger: "blur" },
-        { min: 3, max: 12, message: "账号长度在 3 到 12 个字符", trigger: "blur" },
+        { min: 3, max: 12, message: "账号长度为 3 到 12 个字符", trigger: "blur" },
     ],
     password: [
         { required: true, message: "请输入密码", trigger: "blur" },
-        { min: 6, max: 18, message: "密码长度在 6 到 18 个字符", trigger: "blur" },
+        { min: 6, max: 18, message: "密码长度为 6 到 18 个字符", trigger: "blur" },
     ],
 };
 // 管理员登录数据 
